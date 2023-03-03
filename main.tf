9bf3906bbe0cb90d4c947162ba690d07b0600f7e
salma <salma.ahmed0075@gmail.com>
2023-03-03 16:07:34 +0200

    adding tfstate file to s3 bucket

--- main.tf
+++ main.tf
@@ -1,13 +1,31 @@
-terraform {
-  required_providers {
-   aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+//ec2 instance
+resource "aws_instance" "my-ec2"{
+    ami="ami-0c0933ae5caf0f5f9"
+    instance_type="t2.micro"
+
+    tags = {
+        Name = "salmaEc2"
     }
+}
+
+resource  "aws_eip" "my-eip"{
+    vpc = true
+}
+
+resource "aws_eip_association" "associate"{
+    instance_id=aws_instance.my-ec2.id
+    allocation_id=aws_eip.my-eip.id
+
+}
+
+resource "aws_security_group" "sg" {
+  tags = {
+    Name = "test-sg"
+    type = "terraform-test-security-group"
   }
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "eu-central-1"
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = aws_security_group.sg.id
+  network_interface_id = aws_instance.my-ec2.primary_network_interface_id
 }
